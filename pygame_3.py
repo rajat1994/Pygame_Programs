0804a5678fa086ee325c20804f08f5cae20199b5
rajat1994 <rajatarora25@yahoo.in>
2015-10-17 01:28:18 +0530

    how to load image

---
+++ pygame_3.py
@@ -0,0 +1,52 @@
+
+# coding: utf-8
+
+# In[9]:
+
+import pygame
+import sys
+from pygame.locals import *
+
+pygame.init()
+
+white = (255,255,255)
+black =(0,0,0)
+red =(255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+yellow = (255,255,0)
+cyan = (0,255,255)
+purple = (255,0,255)
+
+setDisplay = pygame.display.set_mode((400,300))
+pygame.display.set_caption('epic game')
+
+img = pygame.image.load('sample-tileset.png')
+img_x=50
+img_y=50
+
+while True :
+   setDisplay.blit(img,(img_x,img_y))
+   for event in pygame.event.get() :
+       if event.type == QUIT:
+          pygame.quit()
+          sys.exit()
+
+   pygame.display.update()
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
